f1bd2b9c55867ee4c71574fe9d592fcd4ce5cdd3
Diogo Baeder de Paula Pinto <diogo.pinto@gonow.com.br>
2010-08-18 20:51:29 -0300

    improvement of the tests; let them pass by receiving errors where expected

--- test/tests.py
+++ test/tests.py
@@ -1,11 +1,21 @@
-from core import *
+import core # just a placeholder to put production code
 
+from nose.tools import raises
+
+# method tests
 class TestExample(object):
 
     def test_should_have_success(self):
-        assert willSucceed()
-
+        assert core.willSucceed()
+    
+    @raises(AssertionError)
     def test_should_fail(self):
-        assert willFail(), "Failures will be notified. Comment this assertion to see all green again."
-        assert willRaiseException()
-        assert willSucceed()
+        assert core.willFail()
+    
+    @raises(Exception)
+    def test_should_raise_exception(self):
+        assert core.willRaiseException()
+
+# function tests
+def test_should_have_success():
+    assert core.willSucceed()
