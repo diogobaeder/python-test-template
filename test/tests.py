3236f038519419baa8fa51bd6b3bcb27b9abe4c4
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2011-04-21 02:27:49 +0200

    No start message for notifications

--- test/tests.py
+++ test/tests.py
@@ -7,11 +7,11 @@ class TestExample(object):
 
     def test_should_have_success(self):
         assert core.willSucceed()
-    
+
     @raises(AssertionError)
     def test_should_fail(self):
         assert core.willFail()
-    
+
     @raises(Exception)
     def test_should_raise_exception(self):
         assert core.willRaiseException()
